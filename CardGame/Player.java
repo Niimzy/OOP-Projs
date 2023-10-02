fe295a6eeef885280c57b2e62146cb45122c6299
Azaz <127027685+Niimzy@users.noreply.github.com>
2023-10-02 10:08:19 +0400

    Update Player.java

--- CardGame/Player.java
+++ CardGame/Player.java
@@ -1,13 +1,17 @@
 public class Player {
+    final String name;
+    final int score;
 
-
-
-    Player(){
-            String[] name = {"Ali", "Ahmed", "Saad", "BRub"};
-
+    Player(String name, int score) {
+        this.name = name;
+        this.score = score;
     }
-
-
+    public int getScore() {
+        return score;
+    }
+    public String getName() {
+        return name;
+    }
 
 
 }
